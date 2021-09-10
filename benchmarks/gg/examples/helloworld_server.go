8009ca4b157595f5e2d0dcfe2ac42f87881d98ac
MBaczun <s1743497@ed.ac.uk>
2021-09-10 16:21:59 +0100

    Adds gg driver and examples
    Made example run scripts and gg driver function, docker, and kn
    manifest.
    
    Signed-off-by: Michal Baczun <s1743497@ed.ac.uk>

---
+++ benchmarks/gg/examples/helloworld_server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received request : %v", in.GetName())
+
+	log.Printf("trying to run gg")
+	cmd := exec.Command("/bin/bash", "/app/fibonacci/bin/run-vhive.sh", "10", "1")
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf(fmt.Sprint(err) + ": " + stderr.String())
+	}
+	log.Printf("Result: " + out.String())
+	log.Printf("Result: " + stderr.String())
+
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
