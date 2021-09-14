e94133c0af67821fe3d1f71dadec57e095dd3cff
MBaczun <s1743497@ed.ac.uk>
2021-09-14 13:49:39 +0100

    Fixes llvm and excamera scripts.
    Signed-off-by: Michal Baczun <s1743497@ed.ac.uk>

--- benchmarks/gg/examples/fibonacci/bin/run-vhive.sh
+++ benchmarks/gg/examples/fibonacci/bin/run-vhive.sh
@@ -8,8 +8,8 @@ JOBS_COUNT=${3?$USAGE}
 
 N="10"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/../
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# cd $DIR/../
 
 cd /app/fibonacci
 
