bc73e37b77a8948d000daa2bedbfde4f7f2850f8
Harshit Garg <hg1229@gmail.com>
2022-02-17 22:03:31 +0530

    Add Auth function
    Working: docker locally (python & go), knative (python & go), zipkin for
    go

---
+++ benchmarks/auth/go/go.mod
@@ -0,0 +1,10 @@
+module server
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200624020401-64a14ca9d1ad // indirect
+	google.golang.org/grpc v1.29.1
+)
---
+++ benchmarks/auth/python/server.py
@@ -0,0 +1,89 @@
+from concurrent import futures
+import logging
+
+import grpc
+
+import random
+import string
+
+import os
+import sys
+
+# sys.path.append('../proto')  # for local testing (i.e. not running in Docker-compose)
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+import ctypes
+libc = ctypes.CDLL(None)
+syscall = libc.syscall
+
+print("python version: %s" % sys.version)
+print("Server has PID: %d" % os.getpid())
+GRPC_PORT_ADDRESS = os.getenv("GRPC_PORT")
+
+
+
+class Empty:  # declare this class to enable dynamically adding new attributes
+    pass
+
+def generatePolicy(principalId, effect, resource):
+    authResponse = Empty()
+
+    authResponse.principalId = principalId
+    if effect and resource:
+        policyDocument = Empty()
+        policyDocument.Version = '2012-10-17'
+        policyDocument.Statement = [None]
+        statementOne = Empty()
+        statementOne.Action = 'execute-api:Invoke'
+        statementOne.Effect = effect
+        statementOne.Resource = resource
+        policyDocument.Statement[0] = statementOne
+        authResponse.policyDocument = policyDocument
+    
+    # Optional output with custom properties of the String, Number or Boolean type.
+    authResponse.context = {
+      "stringKey": "stringval",
+      "numberKey": 123,
+      "booleanKey": True
+    }
+
+    return authResponse
+
+
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+
+        token = request.name
+        fakeMethodArn = "arn:aws:execute-api:{regionId}:{accountId}:{apiId}/{stage}/{httpVerb}/[{resource}/[{child-resources}]]"
+        
+        if '.f2' in token:
+            generatePolicy('user', 'Allow', fakeMethodArn)
+            msg = 'auth.f2'
+        else:
+            generatePolicy('user', 'Deny', fakeMethodArn)
+            msg = 'auth.f1'
+
+        gid = syscall(104)
+        msg = "Serve Function: Python.%s: from GID: %i" % (msg, gid)
+        return helloworld_pb2.HelloReply(message=msg)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+
+    address = ('[::]:' + GRPC_PORT_ADDRESS if GRPC_PORT_ADDRESS else  '[::]:50051')
+    server.add_insecure_port(address) 
+    print("Start server: listen on : " + address)
+
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
