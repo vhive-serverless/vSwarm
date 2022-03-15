26244dd98dd545845e5989f25628ccd4deb1777c
Harshit Garg <hg1229@gmail.com>
2022-03-15 17:52:32 +0000

    Add aes standalone function benchmark

---
+++ benchmarks/aes/README.md
@@ -0,0 +1,67 @@
+# AES Benchmark
+
+The AES benchmark use the AES128 algorithm to encrypt a certain message string. As we are using the helloworld gRPC protocol for this benchmark the message will be the `name` variable. If you do not specify a name a default name is taken. You can change the default name with the argument `default-plaintext`.
+
+AES requires a secrete key for encryption. The functions use a default key but you can specify your own by passing it with the `key` argument to the function. See source code for more details.
+
+The same functionality is implemented in different runtimes, namely Python, NodeJS and golang.
+
+
+
+
+
+## Running this benchmark locally (using docker)
+
+The detailed and general description how to run benchmarks local you can find [here](../../docs/running_locally.md). The following steps show it on the aes-python function.
+1. Build or pull the function images using `make all-images` or `make pull`.
+2. Start the function with docker-compose
+   ```bash
+   docker-compose -f yamls/docker-compose/dc-aes-<runtime>.yaml up
+   ```
+3. In a new terminal, invoke the interface function with grpcurl. To provide the helloworld protocol explicitly we'll use `-import-path <path/to proto/dir> -proto helloworld.proto`.
+   ```bash
+   ../../tools/bin/grpcurl -plaintext -import-path proto -proto helloworld.proto localhost:50051 helloworld.Greeter.SayHello
+   ```
+4. Run the invoker
+   ```bash
+   # build the invoker binary
+   cd ../../tools/invoker
+   make invoker
+
+   # Specify the hostname through "endpoints.json"
+   echo '[ { "hostname": "localhost" } ]' > endpoints.json
+
+   # Start the invoker with a chosen RPS rate and time
+   ./invoker -port 50051 -dbg -time 10 -rps 1
+   ```
+
+
+## Running this benchmark (using knative)
+
+The detailed and general description how to run benchmarks on knative clusters you can find [here](../../docs/running_benchmarks.md). The following steps show it on the aes-python function.
+1. Build or pull the function images using `make all-images` or `make pull`.
+2. Start the function with knative
+   ```bash
+   kn service apply -f ./knative_yamls/aes-python.yaml
+   ```
+3. **Note the URL provided in the output. The part without the `http://` we'll call `$URL`. Replace any instance of `$URL` in the code below with it.**
+4. In a new terminal, invoke the interface function with grpcurl. To provide the helloworld protocol explicitly we'll use `-import-path <path/to proto/dir> -proto helloworld.proto`.
+   ```bash
+    ../../tools/bin/grpcurl -plaintext -import-path proto -proto helloworld.proto $URL:50051 helloworld.Greeter.SayHello
+   ```
+5. Run the invoker
+   ```bash
+   # build the invoker binary
+   cd ../../tools/invoker
+   make invoker
+
+   # Specify the hostname through "endpoints.json"
+   echo '[ { "hostname": "$URL" } ]' > endpoints.json
+
+   # Start the invoker with a chosen RPS rate and time
+   ./invoker -port 50051 -dbg -time 10 -rps 1
+   ```
+## Tracing
+This Benchmark supports distributed tracing for all runtimes. For the general use see vSwarm docs for tracing [locally](../../docs/running_locally.md#tracing) and with [knative](../../docs/running_benchmarks.md#tracing).
+
+
---
+++ benchmarks/aes/go/server.go
@@ -0,0 +1,167 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"flag"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	//	"log"
+	"net"
+
+	pb "aes/proto"
+
+	tracing "github.com/ease-lab/vSwarm/utils/tracing/go"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	zipkin       = flag.String("zipkin", "http://localhost:9411/api/v2/spans", "zipkin url")
+	address      = flag.String("addr", "0.0.0.0:50051", "Address:Port the grpc server is listening to")
+	key_string   = flag.String("key", "6368616e676520746869732070617373", "The key which is used for encryption")
+	default_name = flag.String("default-plaintext", "exampleplaintext", "Default plaintext when the function is called with the name world")
+)
+
+func AESModeCBC(plaintext []byte) []byte {
+	// Reference: cipher documentation
+	// https://golang.org/pkg/crypto/cipher/#BlockMode
+
+	key, _ := hex.DecodeString(*key_string)
+
+	// CBC mode works on blocks so plaintexts may need to be padded to the
+	// next whole block. For an example of such padding, see
+	// https://tools.ietf.org/html/rfc5246#section-6.2.3.2.
+	var padding [aes.BlockSize]byte
+	if len(plaintext)%aes.BlockSize != 0 {
+		plaintext = append(plaintext, padding[(len(plaintext)%aes.BlockSize):]...)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// IV: initialization vector that randomly.
+	// Need to be size 16. We will use 0 for now
+	iv := make([]byte, aes.BlockSize)
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext
+}
+
+func AESModeCTR(plaintext []byte) []byte {
+	// Reference: cipher documentation
+	// https://golang.org/pkg/crypto/cipher/#Stream
+
+	key, _ := hex.DecodeString(*key_string)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	// We will use 0 to be predictable
+	iv := make([]byte, aes.BlockSize)
+	ciphertext := make([]byte, len(plaintext))
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(ciphertext, plaintext)
+	return ciphertext
+}
+
+// func f1() (string, []byte, []byte) {
+// 	plaintext := []byte("exampleplaintext")
+// 	ciphertext := AESModeCTR(plaintext)
+// 	return "GoLang.aes.f1", plaintext, ciphertext
+// }
+// func f2() (string, []byte, []byte) {
+// 	plaintext := []byte("a m e s s a g e ")
+// 	ciphertext := AESModeCTR(plaintext)
+// 	return "GoLang.aes.f2", plaintext, ciphertext
+// }
+// func fn(plaintext []byte) (string, []byte, []byte) {
+// 	ciphertext := AESModeCTR(plaintext)
+// 	return "GoLang.aes.fn(plaintext)", plaintext, ciphertext
+// }
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	// log.Printf("Received: %v", in.GetName())
+
+	var plaintext, ciphertext []byte
+	if in.GetName() == "" || in.GetName() == "world" {
+		plaintext = []byte(*default_name)
+	} else {
+		plaintext = []byte(in.GetName())
+	}
+	// Do the encryption
+	ciphertext = AESModeCTR(plaintext)
+	resp := fmt.Sprintf("fn: AES | plaintext: %s | ciphertext: %x | runtime: golang", plaintext, ciphertext)
+	return &pb.HelloReply{Message: resp}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if tracing.IsTracingEnabled() {
+		log.Printf("Start tracing on : %s\n", *zipkin)
+		shutdown, err := tracing.InitBasicTracer(*zipkin, "aes function")
+		if err != nil {
+			log.Warn(err)
+		}
+		defer shutdown()
+	}
+
+	lis, err := net.Listen("tcp", *address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("Start AES-go server. Addr: %s\n", *address)
+
+	var grpcServer *grpc.Server
+	if tracing.IsTracingEnabled() {
+		grpcServer = tracing.GetGRPCServerWithUnaryInterceptor()
+	} else {
+		grpcServer = grpc.NewServer()
+	}
+	pb.RegisterGreeterServer(grpcServer, &server{})
+	reflection.Register(grpcServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
