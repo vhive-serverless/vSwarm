d9a07dbfac297412fee0e0e00845a4e695c690d2
David Schall <d.h.schall@sms.ed.ac.uk>
2022-03-18 15:24:12 +0000

    Self-hosted runners are broken Fixes #94
    Signed-off-by: David Schall <d.h.schall@sms.ed.ac.uk>

---
+++ runner/scripts/01-kind.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+kindVersion=$(kind version);
+K8S_VERSION=${k8sVersion:-v1.21.10}
+CLUSTER_NAME=${KIND_CLUSTER_NAME:-knative}
+KIND_VERSION=${KIND_VERSION:-v0.11}
+
+echo "KinD version is ${kindVersion}"
+if [[ ! $kindVersion =~ "${KIND_VERSION}." ]]; then
+  echo "WARNING: Please make sure you are using KinD version ${KIND_VERSION}.x, download from https://github.com/kubernetes-sigs/kind/releases"
+  echo "For example if using brew, run: brew upgrade kind"
+  read -p "Do you want to continue on your own risk? Y/n: " REPLYKIND </dev/tty
+  if [ "$REPLYKIND" == "Y" ] || [ "$REPLYKIND" == "y" ] || [ -z "$REPLYKIND" ]; then
+    echo "You are very brave..."
+    sleep 2
+  elif [ "$REPLYKIND" == "N" ] || [ "$REPLYKIND" == "n" ]; then
+    echo "Installation stopped, please upgrade kind and run again"
+    exit 0
+  fi
+fi
+
+REPLY=continue
+KIND_EXIST="$(kind get clusters -q | grep ${CLUSTER_NAME} || true)"
+if [[ ${KIND_EXIST} ]] ; then
+ read -p "Knative Cluster kind-${CLUSTER_NAME} already installed, delete and re-create? N/y: " REPLY </dev/tty
+fi
+if [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; then
+  kind delete cluster --name ${CLUSTER_NAME}
+elif [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ] || [ -z "$REPLY" ]; then
+  echo "Installation skipped"
+  exit 0
+fi
+
+KIND_CLUSTER=$(mktemp)
+cat <<EOF | kind create cluster --name ${CLUSTER_NAME} --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:${K8S_VERSION}
+EOF
+echo "Waiting on cluster to be ready..."
+sleep 10
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kube-system > /dev/null
---
+++ runner/scripts/02-serving.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -u
+
+KNATIVE_VERSION=${KNATIVE_VERSION:-0.23.0}
+
+n=0
+set +e
+until [ $n -ge 2 ]; do
+  kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-crds.yaml > /dev/null && break
+  n=$[$n+1]
+  sleep 5
+done
+set -e
+kubectl wait --for=condition=Established --all crd > /dev/null
+
+n=0
+set +e
+until [ $n -ge 2 ]; do
+  kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-core.yaml > /dev/null && break
+  n=$[$n+1]
+  sleep 5
+done
+set -e
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving > /dev/null
+
+
