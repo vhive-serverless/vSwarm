995978a70f053d0ac1b193c065aef9fee6b5aaef
Harshit Garg <hg1229@gmail.com>
2022-03-24 14:12:59 +0000

    Update files according to PR requirements
    Signed-off-by: Harshit Garg <hg1229@gmail.com>

--- benchmarks/aes/python/server.py
+++ benchmarks/aes/python/server.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2022 EASE lab
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from concurrent import futures
 import logging
 
@@ -20,7 +42,6 @@ import helloworld_pb2_grpc
 sys.path.insert(0, os.getcwd() + '/../../../utils/tracing/python')
 import tracing
 
-# USE ENV VAR "DecoderFrames" to set the number of frames to be sent
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--addr", dest="addr", default="0.0.0.0", help="IP address")
 parser.add_argument("-p", "--port", dest="port", default="50051", help="serve port")
@@ -94,5 +115,4 @@ def serve():
 
 
 if __name__ == '__main__':
-    # logging.basicConfig()
     serve()
