2bd50b8441856ffefb4a8f2a82aeda4cb1e3798c
Harshit Garg <hg1229@gmail.com>
2022-05-12 12:24:26 +0100

    Update fibonacci for tracing
    Signed-off-by: Harshit Garg <hg1229@gmail.com>

--- benchmarks/fibonacci/nodejs/server.js
+++ benchmarks/fibonacci/nodejs/server.js
@@ -22,40 +22,74 @@
 
 // Include process module
 const process = require('process');
-const GRPC_PORT = process.env.GRPC_PORT || '50051'
-const version = process.version
-
+const tracing = require('tracing')
 
 var PROTO_PATH = __dirname + '/fibonacci.proto';
 
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).fibonacci;
 
 
+function fibonacci(num) {
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num1;
+}
 
+/**
+ * Argument parsing
+ */
+function parsArgs() {
 
-function fibonacci(num)
-{
-    var num1=0;
-    var num2=1;
-    var sum;
-    var i=0;
-    for (i = 0; i < num; i++) 
-    {
-        sum=num1+num2;
-        num1=num2;
-        num2=sum;
-    }
-    return num1;
+  var { argv } = require("yargs")
+    .scriptName("server")
+    .usage("Usage: $0 --addr <IP> --port <PORT> --zipkin <ZIPKIN URL>")
+    .example(
+      "$0 -p 50051",
+      "Starts Fibonacci gRPC server on localhost:50051"
+    )
+    .option("a", {
+      alias: "addr", type: "string",
+      describe: "IP address",
+    })
+    .option("p", {
+      alias: "port", type: "string",
+      describe: "Port the server listen to",
+    })
+    .option("z", {
+      alias: "zipkin", type: "string",
+      describe: "Zipkin URL",
+    })
+    .describe("help", "Show help.") // Override --help usage message.
+    .describe("version", "Show version number.") // Override --version usage message.
+    .epilog("copyright 2022");
+
+  var addr = "0.0.0.0";
+  var port = "50051";
+  var zipkin = "http://localhost:9411/api/v2/spans";
+
+  if ('addr' in argv) { addr = argv.addr; }
+  if ('port' in argv) { port = argv.port; }
+  if ('zipkin' in argv) { zipkin = argv.zipkin; }
+
+  return [addr, port, zipkin];
 }
 
 
@@ -64,11 +98,22 @@ function fibonacci(num)
  * Implements the SayHello RPC method.
  */
 function sayHello(call, callback) {
-  
+  process.stdout.write(`Received gRPC call.\n`);
+
+  if (tracing.IsTracingEnabled()) {
+    span = new tracing.Span()
+  }
+
   var x = parseInt(call.request.name)
   var y = fibonacci(x)
   var msg = `fn: Fib: y = fib(x) | x: ${x} y: ${y} | runtime: NodeJS`
-  callback(null, {message: msg});
+
+  if (tracing.IsTracingEnabled()) {
+    span.addEvent(msg);
+    span.end();
+  }
+
+  callback(null, { message: msg });
 }
 
 /**
@@ -76,15 +121,21 @@ function sayHello(call, callback) {
  * sample server port
  */
 function main() {
+  const [addr, port, zipkin] = parsArgs();
+
+  if (tracing.IsTracingEnabled()) {
+    process.stdout.write(`Tracing enabled\n`);
+    tracing.InitTracer('fibonacci-nodejs-server', zipkin);
+  } else {
+    process.stdout.write(`Tracing disabled\n`);
+  }
+
   var server = new grpc.Server();
-  process.stdout.write(`Node version: ${version}\n`);
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  // address = '[::]:' + GRPC_PORT
-  address = '0.0.0.0:' + GRPC_PORT
+  server.addService(hello_proto.Greeter.service, { sayHello: sayHello });
+  address = `${addr}:${port}`
   server.bindAsync(
     address,
     grpc.ServerCredentials.createInsecure(),
-    // grpc.ServerCredentials.createSsl(),
     (err, port) => {
       process.stdout.write(`Server started on ${address}\n`);
       server.start();
--- utils/tracing/integ-tests/client-server/goclient/go.mod
+++ utils/tracing/integ-tests/client-server/goclient/go.mod
@@ -4,8 +4,8 @@ go 1.16
 
 require (
 	github.com/containerd/containerd v1.6.2
-	github.com/ease-lab/vSwarm-proto v0.0.0-20220425132824-72aa8060e781
-	github.com/ease-lab/vSwarm/utils/tracing/go v0.0.0-20220425145559-091dd7ccf6e7
+	github.com/ease-lab/vSwarm-proto v0.1.3
+	github.com/ease-lab/vSwarm/utils/tracing/go v0.0.0-20220427112636-f8f3fc171804
 	github.com/sirupsen/logrus v1.8.1
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.31.0
 	google.golang.org/grpc v1.46.0
