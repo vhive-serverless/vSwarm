37d610997911c90ff08af308e86e86af1e1e9a2a
Dohyun Park <dohyun1357@gmail.com>
2022-07-13 11:36:46 +0200

    clean up redundant/optional code, add tests
    Signed-off-by: Dohyun Park <dohyun1357@gmail.com>

--- tools/trace-plotter/loader/loader.go
+++
@@ -1,9 +0,0 @@
-package loader
-
-import (
-	"github.com/ease-lab/vSwarm/tools/trace-plotter/models"
-)
-
-type Loader interface {
-	GetTraces(pageSize int, traces *[]models.Trace)
-}
--- tools/trace-plotter/main.go
+++ tools/trace-plotter/main.go
@@ -1,85 +1,72 @@
+// MIT License
+//
+// Copyright (c) 2022 Dohyun Park and EASE lab
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package main
 
 import (
 	"flag"
-	"net/http"
 	"os"
-	"strconv"
+	"path/filepath"
 
-	"github.com/ease-lab/vSwarm/tools/trace-plotter/loader"
-	"github.com/ease-lab/vSwarm/tools/trace-plotter/models"
-	"github.com/ease-lab/vSwarm/tools/trace-plotter/plotter"
-
-	"github.com/elastic/go-elasticsearch/v7"
+	elasticsearch "github.com/elastic/go-elasticsearch/v7"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	useZipkinApi     = flag.Bool("useZipkinApi", false, "use zipkin api")
 	elasticsearchURL = flag.String("elasticsearchURL", "http://127.0.0.1:9200", "Elasticsearch URL")
-	pageSize         = flag.Int("pageSize", 100, "Trace request page size")
+	pageSize         = flag.Int("pageSize", 100, "The number of traces to fetch per page while paginating")
 	zipkinURL        = flag.String("zipkinURL", "http://127.0.0.1:8080", "Zipkin URL")
-	asWebServer      = flag.Bool("asWebServer", false, "Run as a webserver")
+	fileName         = flag.String("fileName", "plot.html", "output file name")
+	latencyType      = flag.String("latencyType", "e2e", "which latency type to plot, e2e or system(e2e - leaf trace execution time)")
 )
 
 func main() {
 	flag.Parse()
-	var l loader.Loader
-	if *useZipkinApi {
-		log.Info("Using zipkin api")
-		l = loader.NewZipkinApiLoader(*zipkinURL)
-	} else {
-		log.Info("Using elasticsearch api")
-		es, err := elasticsearch.NewClient(elasticsearch.Config{
-			Addresses: []string{*elasticsearchURL},
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		l = loader.NewEsLoader(es)
+	switch *latencyType {
+	case "e2e", "system":
+	default:
+		log.Fatalf("latencyType must be one of e2e or system")
 	}
 
-	if *asWebServer {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			var (
-				traces []models.Trace
-				size   = *pageSize
-				url    = *zipkinURL
-			)
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{*elasticsearchURL},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	l := NewLoader(es)
 
-			if r.FormValue("pageSize") != "" {
-				s, err := strconv.Atoi(r.FormValue("pageSize"))
-				if err != nil {
-					log.Errorf("Error parsing page size: %s", err)
-				}
-				size = s
-			}
+	var traces []Trace
+	l.GetTraces(*pageSize, &traces)
 
-			if r.FormValue("zipkinURL") != "" {
-				url = r.FormValue("zipkinURL")
-			}
+	parsedTraces, durations := ParseTraces(traces, *latencyType)
+	scatter := PlotGraph(parsedTraces, durations, *zipkinURL)
 
-			l.GetTraces(size, &traces)
-
-			rootTraces, durations := plotter.ParseTraces(traces)
-			scatter := plotter.PlotGraph(rootTraces, durations, url)
-
-			scatter.Render(w)
-		})
-		log.Info("Starting server :8081")
-		err := http.ListenAndServe(":8081", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		var traces []models.Trace
-		l.GetTraces(*pageSize, &traces)
-
-		rootTraces, durations := plotter.ParseTraces(traces)
-		scatter := plotter.PlotGraph(rootTraces, durations, *zipkinURL)
-
-		f, _ := os.Create("plot.html")
-		scatter.Render(f)
+	if filepath.Ext(*fileName) != ".html" {
+		*fileName += ".html"
+	}
+	f, _ := os.Create(*fileName)
+	if err := scatter.Render(f); err != nil {
+		log.Errorf("Error rendering plot: %s", err)
 	}
 }
