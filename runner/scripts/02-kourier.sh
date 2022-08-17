30788a5abd99a25bee474b2d4a61cbab8041ab4b
David Schall <david.schall@ed.ac.uk>
2022-08-17 10:40:14 -0600

    Clean up and minor version fixes
    Signed-off-by: David Schall <david.schall@ed.ac.uk>

--- runner/scripts/02-kourier.sh
+++ runner/scripts/02-kourier.sh
@@ -3,18 +3,18 @@
 set -eo pipefail
 set -u
 
-KNATIVE_NET_KOURIER_VERSION=${KNATIVE_NET_KOURIER_VERSION:-1.2.0}
+KNATIVE_NET_KOURIER_VERSION=${KNATIVE_NET_KOURIER_VERSION:-1.4.0}
 
 ## INSTALL KOURIER
 n=0
 until [ $n -ge 3 ]; do
-  kubectl apply -f https://github.com/knative-sandbox/net-kourier/releases/download/knative-v${KNATIVE_NET_KOURIER_VERSION}/kourier.yaml > /dev/null && break
+  kubectl apply -f https://github.com/knative-sandbox/net-kourier/releases/download/knative-v${KNATIVE_NET_KOURIER_VERSION}/kourier.yaml  && break
   echo "Kourier failed to install on first try"
   n=$[$n+1]
   sleep 10
 done
-kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kourier-system > /dev/null
-kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving > /dev/null
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kourier-system
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving
 
 # Configure Knative to use this ingress
 kubectl patch configmap/config-network \
