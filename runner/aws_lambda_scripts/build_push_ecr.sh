4ca3fb899cf7f9b425d377fac007cba50f4e4a46
Alan Nair <alannair@iitk.ac.in>
2022-11-04 22:42:45 +0530

    Updated Copyright
    Signed-off-by: Alan Nair <alannair@iitk.ac.in>

--- runner/aws_lambda_scripts/build_push_ecr.sh
+++ runner/aws_lambda_scripts/build_push_ecr.sh
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2021 Michal Baczun and EASE lab
+# Copyright (c) 2022 Alan Nair and The vHive Ecosystem
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
