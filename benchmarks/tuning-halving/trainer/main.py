09ab6c65f8ed706912e6a9985666f0f056f79c48
jingren1021 <jingren1021@outlook.com>
2022-11-10 17:16:38 +0900

    add support for BUCKET_NAME env variable for tuning halving benchmark
    Signed-off-by: jingren1021 <jingren1021@outlook.com>

--- benchmarks/tuning-halving/trainer/main.py
+++ benchmarks/tuning-halving/trainer/main.py
@@ -77,7 +77,8 @@ storageBackend = None
 # set aws credentials:
 AWS_ID = os.getenv('AWS_ACCESS_KEY', "")
 AWS_SECRET = os.getenv('AWS_SECRET_KEY', "")
-
+# set aws bucket name:
+BUCKET_NAME = os.getenv('BUCKET_NAME','vhive-tuning')
 
 def get_self_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -110,7 +111,7 @@ def model_dispatcher(model_name):
 class TrainerServicer(tuning_pb2_grpc.TrainerServicer):
     def __init__(self, transferType, XDTconfig=None):
 
-        self.benchName = 'vhive-tuning'
+        self.benchName = BUCKET_NAME
         self.transferType = transferType
         self.trainer_id = ""
         if transferType == S3:
@@ -171,7 +172,7 @@ def serve():
     transferType = os.getenv('TRANSFER_TYPE', S3)
     if transferType == S3:
         global storageBackend
-        storageBackend = Storage('vhive-tuning')
+        storageBackend = Storage(BUCKET_NAME)
         log.info("Using inline or s3 transfers")
         max_workers = int(os.getenv("MAX_SERVER_THREADS", 10))
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
