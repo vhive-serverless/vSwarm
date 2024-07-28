c0fc4fa764fe38f459af038b8e7baed039b8f61b
Lakshman <Lakshman@localhost>
2024-07-28 00:05:27 -0600

    Upgraded fibonacci to deal with large numbers.
    Signed-off-by: L Lakshmanan <lakshmanan.l@research.iiit.ac.in>

--- benchmarks/fibonacci/nodejs/server.js
+++ benchmarks/fibonacci/nodejs/server.js
@@ -41,16 +41,16 @@ var hello_proto = grpc.loadPackageDefinition(packageDefinition).fibonacci;
 
 
 function fibonacci(num) {
-  var num1 = 0;
-  var num2 = 1;
-  var sum;
+  let num1 = BigInt(0);
+  let num2 = BigInt(1);
+  let sum = BigInt(0);
   var i = 0;
   for (i = 0; i < num; i++) {
     sum = num1 + num2;
     num1 = num2;
     num2 = sum;
   }
-  return num1;
+  return num1.toString();
 }
 
 /**
